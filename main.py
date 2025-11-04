6635b5ef0d732a6f296f01404855fc0e78fa1e3e
Yuchen Gu <93961613+Guyuchen99@users.noreply.github.com>
2025-11-04 13:32:55 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://ycgraphixs-dev.myshopify.com",
-        "http://127.0.0.1:5500",
+        "https://markv3.myshopify.com",
+        "https://localhost:3458",
     ],
     allow_credentials=True,
     allow_methods=["*"],
